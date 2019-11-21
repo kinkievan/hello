ab6b2708c7b5c1dec85351c3aa77da902aad3bf1
Kiki.Lai@nttdata.com <kinkievan@hotmail.com>
2019-11-21 17:03:43 +0800

    test

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+import logging,sys
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+filelog = True
+path = r'log.txt'
+
+logger = logging.getLogger('log')
+logger.setLevel(logging.DEBUG)
+
+# ?用模??,如果??引用，比如多次?用，每次?添加Handler，造成重复日志，??每次都移除掉所有的handler，后面在重新添加，可以解?????
+while logger.hasHandlers():
+    for i in logger.handlers:
+        logger.removeHandler(i)
+
+# file log
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+if filelog:
+    fh = logging.FileHandler(path,encoding='utf-8')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+# console log
+formatter = logging.Formatter('%(message)s')
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    logger.info("?是一???")
