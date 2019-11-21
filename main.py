a941f6202016244e521848661edc028e24ed377a
Kiki.Lai@nttdata.com <kinkievan@hotmail.com>
2019-11-21 17:37:01 +0800

    ----

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  
-    return 'Hel000000000000lo World!'
+def hello_world():
+    return 'Hello Wor000000000000000d!'
+
 
 if __name__ == '__main__':
     logger.info("賴炘玫測試")
