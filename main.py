488a4095d2f5a12cd8463070bf45a546c8ad45c7
Kiki.Lai@nttdata.com <kinkievan@hotmail.com>
2019-11-21 17:14:36 +0800

    ??

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-﻿import logging,sys
+﻿from flask import Flask
+app = Flask(__name__)
 
-filelog = True
-path = r'log.txt'
-
-logger = logging.getLogger('log')
-logger.setLevel(logging.DEBUG)
-
-
-while logger.hasHandlers():
-    for i in logger.handlers:
-        logger.removeHandler(i)
-
-# file log
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-if filelog:
-    fh = logging.FileHandler(path,encoding='utf-8')
-    fh.setLevel(logging.DEBUG)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-
-# console log
-formatter = logging.Formatter('%(message)s')
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    logger.info("賴炘玫")
+    app.run()
