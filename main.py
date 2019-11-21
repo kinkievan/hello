1a44bb5f4c5e33817e3ed6d78b29f77dee079c32
Kiki.Lai@nttdata.com <kinkievan@hotmail.com>
2019-11-21 17:20:01 +0800

    dddd

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    logging.FileHandler(filename='translator.log', encoding='utf-8')
     return 'Hello World!'
 
 if __name__ == '__main__':
