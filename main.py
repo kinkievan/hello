789ffde73f771c45bd288be181472012e56a5759
Kiki.Lai@nttdata.com <kinkievan@hotmail.com>
2019-11-21 17:36:03 +0800

    00000

--- main.py
+++ main.py
@@ -5,26 +5,7 @@ os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 from flask import Flask
-app = Flask(__name__)
-import logging,sys
-filelog = True
-path = r'log.txt'
-logger = logging.getLogger('log')
-logger.setLevel(logging.DEBUG)
-while logger.hasHandlers():
-    for i in logger.handlers:
-        logger.removeHandler(i)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-if filelog:
-    fh = logging.FileHandler(path,encoding='utf-8')
-    fh.setLevel(logging.DEBUG)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-formatter = logging.Formatter('%(message)s')
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+
 @app.route('/')
 def hello_world():  
     return 'Hel000000000000lo World!'
